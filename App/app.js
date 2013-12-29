b13ffe39b1e752c162dce42e524086964687ef3e
dnithya@live.com <dnithya@live.com>
2013-12-30 00:06:45 +0530

    Added carousel images and Footer layout changes

--- App/app.js
+++ App/app.js
@@ -1,19 +1,18 @@
-﻿var pmModule = angular.module('pmModule', ['ui.bootstrap', 'screenstructureModule', 'ui.bootstrap.carousel',  'ngSanitize']);
+﻿var pmModule = angular.module('pmModule', ['ui.bootstrap', 'screenstructureModule', 'ui.bootstrap.carousel', 'ngSanitize']);
 
-pmModule.controller('homePageCarousalController', ['$scope',function ($scope) {
+pmModule.controller('homePageCarousalController', ['$scope', function ($scope) {
 
     $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function () {
-        var newWidth = 600 + slides.length;
-        slides.push({
-            image: 'http://placekitten.com/1200/300',
-            text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
-              ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-        });
-    };
-    for (var i = 0; i < 4; i++) {
-        $scope.addSlide();
-    }
+    var slides = $scope.slides = [
+    { text: 'Image1', image: ' Images/Carousel/Carousel1.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel2.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel3.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel4.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel5.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel6.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel7.jpg' },
+    { text: 'Image1', image: 'Images/Carousel/Carousel8.jpg' },
 
-}]);
+    ];
+
+} ]);
